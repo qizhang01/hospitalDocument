db928a1a87942129f4f47cbd8c2d28d123607272
qizhang01 <zhishui120@sina.com>
2025-10-15 09:53:51 +0800

    过滤条件Ui

---
+++ src/views/estimateAndScore/options/protectRestrict.js
@@ -0,0 +1,4 @@
+export const restrictReason = ["预防非计划性拔管","有创操作制动","预防伤害行为","防止坠床"]
+export const consciousness = ["清醒","嗜睡","聸忘","昏迷"]
+export const position = ["左手腕","右手腕","左脚踝","右脚踝","左肩部","右肩部","躯干"]
+export const tightness = ["适宜","不适宜","过松"]
